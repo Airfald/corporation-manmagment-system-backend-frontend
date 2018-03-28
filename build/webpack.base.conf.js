2a186ff32861ac0b7d9052a73b1971cdd5e0fa7f
欧贺福 <hefu.ou@meicloud.com>
2018-03-28 20:21:18 +0800

    fixed bug

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'libraries@': resolve('src/libraries'),
+      'shared@': resolve('src/shared'),
     }
   },
   module: {
